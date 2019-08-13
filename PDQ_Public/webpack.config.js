55c2f41a103effc71e12ea01895837fd756fd0aa
Tyler Walker <ty.j.walker686@gmail.com>
2019-08-12 23:08:14 -0600

    webpack config

---
+++ PDQ_Public/webpack.config.js
@@ -0,0 +1,37 @@
+﻿const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, "wwwroot"),
+        filename: "[name].[chunkhash].js",
+        publicPath: "/"
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json", ".html"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,
+                use: "awesome-typescript-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(["wwwroot/*"]),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[chunkhash].css"
+        })
+    ]
+};
